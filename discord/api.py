4f8b840a51fe8fe9dfedb373b3c7dc4ae518471d
Aditya Agarwal <aditya.agarwal.20012712@gmail.com>
2021-10-09 00:49:52 +0530

    api to get common percentage of users between 2 guilds complete

--- discord/api.py
+++ discord/api.py
@@ -1,7 +1,21 @@
+from django.http.response import HttpResponse
 from rest_framework import generics
 
 from .serializers import DiscordGuildSerializer
-
+from .models import Discord_Guild
 
 class CreateDiscordGuild(generics.CreateAPIView):
     serializer_class = DiscordGuildSerializer
+
+def CommonMembersPercentage(request):
+    arr = request.GET['guild_uid1'].split(',')
+    guild_id1 = arr[0]
+    guild_id2 = arr[1].split('=')[1]
+    try:
+        guild1 = Discord_Guild.objects.get(guild_uid=guild_id1)
+        guild2 = Discord_Guild.objects.get(guild_uid=guild_id2)
+        guild1_user_cnt = len(guild1.discord_user_set.all());
+        common_count = len(guild1.discord_user_set.all() & guild2.discord_user_set.all())
+        return HttpResponse((common_count/guild1_user_cnt)*100)
+    except:
+        return HttpResponse("something went wrong")
--- discord/urls.py
+++ discord/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .api import CreateDiscordGuild
+from .api import CreateDiscordGuild, CommonMembersPercentage
 
 
 urlpatterns = [
     path('discord_guild/create', CreateDiscordGuild.as_view(), name='create-discord-guild'),
+    path('discord_match_percentage/', CommonMembersPercentage, name='percentage-of-common-members')
 ]
